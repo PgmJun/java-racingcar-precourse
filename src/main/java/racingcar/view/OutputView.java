a357b3f392d2c277c98bfd43747bf95a6b678211
JUN <chltmdwns96@naver.com>
2022-12-09 17:53:55 +0900

    feat : 기본 출력 메서드 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void printError(Exception error) {
+        System.out.println(error.getMessage());
+    }
+
+    public void print(String message) {
+        System.out.print(message);
+    }
+
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+    public void printEnter() {
+        System.out.println();
+    }
+
+}
+
